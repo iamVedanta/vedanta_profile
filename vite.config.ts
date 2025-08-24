b888458cf6368ecfdc0c8acf623b1410e8aaeac6
iamVedanta <callvedanta@gmail.com>
2025-08-24 19:51:42 +0530

    trying to fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,8 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/vedanta_profile/" : "/",  // 👈 important
+  base: "/",  // ✅ force root (important for Render)
   server: {
     host: "::",
     port: 8080,
