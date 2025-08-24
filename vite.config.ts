1c78de65af538e57a974657aca752c871c429c89
iamVedanta <callvedanta@gmail.com>
2025-08-24 19:25:28 +0530

    builttt

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/iamVedanta/" : "/",  // 👈 important
+  base: mode === "production" ? "/vedanta_profile/" : "/",  // 👈 important
   server: {
     host: "::",
     port: 8080,
